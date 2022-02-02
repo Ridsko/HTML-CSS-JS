00b7c464cea6e727e82944ecb6e0bf13c297f796
Rida <targetagar@gmail.com>
2022-02-02 12:29:54 +0100

    yo

---
+++ Javascript/console.js
@@ -0,0 +1,9 @@
+const junkfood = false;
+const healthyFood = true;
+if(!junkfood){
+    console.log("Good for you")
+}else if (healthyFood && !junkfood){
+    console.log("Great")
+}else{
+    console.log("Dont know what to say");
+}
